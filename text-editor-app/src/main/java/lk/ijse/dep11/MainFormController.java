7612a92c232cfb91d388537b729a04d89ae62f45
Tharindu Ishan <ishanthari96@gmail.com>
2023-08-21 23:41:50 +0530

    About Button Added

--- text-editor-app/src/main/java/lk/ijse/dep11/MainFormController.java
+++ text-editor-app/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -5,8 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
 import javafx.scene.web.HTMLEditor;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -16,6 +20,7 @@ public class MainFormController {
     public HTMLEditor htmlEditor;
     public MenuItem btnClose;
     public MenuItem btnUserGuide;
+    public MenuItem btnAbout;
 
     public void btnNewOnAction(ActionEvent actionEvent) {
         htmlEditor.setHtmlText("");
@@ -28,8 +33,24 @@ public class MainFormController {
         AnchorPane root= FXMLLoader.load(getClass().getResource("/view/UserGuide.fxml"));
         Scene scene = new Scene(root);
         Stage stage = new Stage();
+        stage.initModality(Modality.APPLICATION_MODAL);
         stage.setScene(scene);
         stage.setTitle("User Guide");
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+    public void btnAboutOnAction(ActionEvent actionEvent) throws IOException {
+        AnchorPane root= FXMLLoader.load(getClass().getResource("/view/About.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.setTitle("About");
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+        root.setBackground(Background.fill(Color.TRANSPARENT));
+        scene.setFill(Color.TRANSPARENT);
+
         stage.centerOnScreen();
         stage.show();
     }

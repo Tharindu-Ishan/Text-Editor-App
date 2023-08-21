9b12f5307aac1ea4e22c0d7a241041d303072c06
Tharindu Ishan <ishanthari96@gmail.com>
2023-08-21 18:37:04 +0530

    1st commit

---
+++ text-editor-app/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -0,0 +1,29 @@
+package lk.ijse.dep11;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        AnchorPane root= FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Text Editor");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+
+
+    }
+}

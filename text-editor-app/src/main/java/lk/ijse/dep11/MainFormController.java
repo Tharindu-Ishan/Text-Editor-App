305918b0976933aa5b3a0d537043539e6b3c46a1
Tharindu Ishan <ishanthari96@gmail.com>
2023-08-21 21:47:11 +0530

    New Button Added

--- text-editor-app/src/main/java/lk/ijse/dep11/MainFormController.java
+++ text-editor-app/src/main/java/lk/ijse/dep11/MainFormController.java
@@ -1,5 +1,15 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuItem;
+import javafx.scene.web.HTMLEditor;
+
 public class MainFormController {
 
+    public MenuItem btnNew;
+    public HTMLEditor htmlEditor;
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+        htmlEditor.setHtmlText("");
+    }
 }
